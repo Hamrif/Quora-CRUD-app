4f9d658c0f1d244135aba4bedda041a07944d7ef
hamrif <hamrif2920@gmail.com>
2026-01-24 19:07:28 -0600

    added deletion functionalities

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app =express();
 const port=3000;
 const path=require("path");
 const {v4:uuidv4}=require("uuid");
+const methodOverride=require("method-override");
 
 
-
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.set("veiw engine","ejs");
+app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 
@@ -50,5 +51,35 @@ app.post("/posts",(req,res)=>{
     let {name,content}=req.body;
     let id=uuidv4();
     posts.push({id,name,content});
+    res.redirect("/posts");
+});
+
+app.get ("/posts/:id",(req,res)=>{
+   let {id}=req.params;
+   let post=posts.find((p)=>id===p.id);
+   res.render("show.ejs",{post});
+
+});
+
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let{id}=req.params;
+    let post=posts.find((p)=>id===p.id);
+    res.render("edit.ejs",{post});
+});
+app.patch("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+
+    let newContent=req.body.content;
+    
+    let post= posts.find((p)=>id===p.id);
+    post.content=newContent;
+    
+    res.redirect("/posts");
+});
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    posts=posts.filter((p)=>id!=p.id);
     res.redirect("/posts");
 });
